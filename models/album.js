319f55bfeab4cb1da8513bae49ceeb0ca1ae632e
Arwa Aloraibi <arwa7oraibi@gmail.com>
2025-11-05 20:46:04 +0300

    added user schema

--- models/album.js
+++ models/album.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
-const userSchema = mongoose.Schema({
-  username: {
+const albumSchema = mongoose.Schema({
+  name: {
     type: String,
     require: true,
   },
@@ -23,6 +23,6 @@ userSchema.set('toJSON', {
   },
 });
 
-const User = mongoose.model('User', userSchema);
+const Album = mongoose.model('Album', albumSchema);
 
-module.exports = User;
+module.exports = Album;
