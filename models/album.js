71d97ee71319548f244280726dec1de712f9453d
Arwa Aloraibi <arwa7oraibi@gmail.com>
2025-11-10 19:58:15 +0300

    Create the album and add the songs to the album directly

--- models/album.js
+++ models/album.js
@@ -9,19 +9,15 @@ const albumSchema = new mongoose.Schema({
     coverImg: {
         type: String,
     },
-    albumId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Album',
-    },
-    songs: {
+    songs: [{
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Song',
-    },
+    }],
     userId: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User',
     },
-}, { timestamps: true });
+});
 
 const Album = mongoose.model('Album', albumSchema);
 
