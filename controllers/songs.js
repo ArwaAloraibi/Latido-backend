2df0c23a64d9bd925af0822a0344af264df9ef52
Arwa Aloraibi <arwa7oraibi@gmail.com>
2025-11-12 18:41:31 +0300

    delted the show all songs & edit the /new route

--- controllers/songs.js
+++ controllers/songs.js
@@ -3,18 +3,10 @@ const router = express.Router();
 
 const Song = require('../models/song');
 
-router.get('/', async (req, res) => {
-  try {
-    const songs = await Song.find({}).populate('userId');
-    res.render('songs/index.ejs', { songs });
-  } catch (error) {
-    console.log(error);
-    res.redirect('/');
-  }
-});
+
 
 router.get('/new', (req, res) => {
-  res.render('songs/new.ejs');
+  res.json({ message: 'Render new song form' });
 });
 
 router.post('/', async (req, res) => {
@@ -31,66 +23,66 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.get('/:id', async (req, res) => {
-  try {
-    const song = await Song.findById(req.params.id).populate('userId');
-    res.render('songs/show.ejs', { song });
-  } catch (error) {
-    console.log(error);
-    res.redirect('/');
-  }
-});
+// router.get('/:id', async (req, res) => {
+//   try {
+//     const song = await Song.findById(req.params.id).populate('userId');
+//     res.render('songs/show.ejs', { song });
+//   } catch (error) {
+//     console.log(error);
+//     res.redirect('/');
+//   }
+// });
 
-router.get('/:id/edit', async (req, res) => {
-  try {
-    const song = await Song.findById(req.params.id);
-    res.render('songs/edit.ejs', { song });
-  } catch (error) {
-    console.log(error);
-    res.redirect('/');
-  }
-});
+// router.get('/:id/edit', async (req, res) => {
+//   try {
+//     const song = await Song.findById(req.params.id);
+//     res.render('songs/edit.ejs', { song });
+//   } catch (error) {
+//     console.log(error);
+//     res.redirect('/');
+//   }
+// });
 
-router.put('/:id', async (req, res) => {
-  try {
-    const song = await Song.findById(req.params.id);
-    if (!song) return res.redirect('/songs');
+// router.put('/:id', async (req, res) => {
+//   try {
+//     const song = await Song.findById(req.params.id);
+//     if (!song) return res.redirect('/songs');
 
-    const ownerId = song.userId ? song.userId.toString() : null;
-    const sessionId = req.session && req.session.user ? req.session.user._id.toString() : null;
+//     const ownerId = song.userId ? song.userId.toString() : null;
+//     const sessionId = req.session && req.session.user ? req.session.user._id.toString() : null;
 
-    if (ownerId && sessionId && ownerId === sessionId) {
-      await Song.findByIdAndUpdate(req.params.id, req.body, { new: true });
-      res.redirect(`/songs/${req.params.id}`);
-    } else {
-      console.log('Permission denied');
-      res.redirect('/songs');
-    }
-  } catch (error) {
-    console.log(error);
-    res.redirect('/');
-  }
-});
+//     if (ownerId && sessionId && ownerId === sessionId) {
+//       await Song.findByIdAndUpdate(req.params.id, req.body, { new: true });
+//       res.redirect(`/songs/${req.params.id}`);
+//     } else {
+//       console.log('Permission denied');
+//       res.redirect('/songs');
+//     }
+//   } catch (error) {
+//     console.log(error);
+//     res.redirect('/');
+//   }
+// });
 
-router.delete('/:id', async (req, res) => {
-  try {
-    const song = await Song.findById(req.params.id);
-    if (!song) return res.redirect('/songs');
+// router.delete('/:id', async (req, res) => {
+//   try {
+//     const song = await Song.findById(req.params.id);
+//     if (!song) return res.redirect('/songs');
 
-    const ownerId = song.userId ? song.userId.toString() : null;
-    const sessionId = req.session && req.session.user ? req.session.user._id.toString() : null;
+//     const ownerId = song.userId ? song.userId.toString() : null;
+//     const sessionId = req.session && req.session.user ? req.session.user._id.toString() : null;
 
-    if (ownerId && sessionId && ownerId === sessionId) {
-      await Song.findByIdAndDelete(req.params.id);
-      res.redirect('/songs');
-    } else {
-      console.log('Permission denied');
-      res.redirect('/songs');
-    }
-  } catch (error) {
-    console.log(error);
-    res.redirect('/');
-  }
-});
+//     if (ownerId && sessionId && ownerId === sessionId) {
+//       await Song.findByIdAndDelete(req.params.id);
+//       res.redirect('/songs');
+//     } else {
+//       console.log('Permission denied');
+//       res.redirect('/songs');
+//     }
+//   } catch (error) {
+//     console.log(error);
+//     res.redirect('/');
+//   }
+// });
 
 module.exports = router;
